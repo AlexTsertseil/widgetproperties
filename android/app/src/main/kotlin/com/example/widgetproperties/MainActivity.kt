363fe0cd95be1c95008d402039228431680cc01a
AlexTsertseil <alex.tsertseil@gmail.com>
2021-07-29 22:11:13 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/widgetproperties/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widgetproperties
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Sanya.jpg'),
+              ),
+              Text(
+                'Sanya',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
